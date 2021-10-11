fb09e6f36be117c97e89ed324658c6fdb1e284e4
mikeLofton <michaellofton32@gmail.com>
2021-10-11 16:54:33 -0500

    We can control our character. Yay!

--- MathForGames/Actor.cs
+++ MathForGames/Actor.cs
@@ -5,9 +5,15 @@ using MathLibrary;
 
 namespace MathForGames
 {
+    struct Icon
+    {
+        public char Symbol;
+        public ConsoleColor Color;
+    }
+
     class Actor
     {
-        private char _icon;
+        private Icon _icon;
         private string _name;
         private Vector2 _position;
         private bool _started;
@@ -26,9 +32,12 @@ namespace MathForGames
             set { _position = value; }
         }
 
-        public Actor(char icon, Vector2 position, string name = "Actor")
+        public Actor(char icon, float x, float y, string name = "Actor", ConsoleColor color = ConsoleColor.White) : 
+            this(icon, new Vector2 { X = x, Y = y}, name, color) {}
+
+        public Actor(char icon, Vector2 position, string name = "Actor", ConsoleColor color = ConsoleColor.White)
         {
-            _icon = icon;
+            _icon = new Icon { Symbol = icon, Color = color };
             _position = position;
             _name = name;
         }
@@ -40,13 +49,12 @@ namespace MathForGames
 
         public virtual void Update()
         {
-            _position.X = Position.X + 1;
+            
         }
 
         public virtual void Draw()
         {
-            Console.SetCursorPosition((int)Position.X, (int)Position.Y);
-            Console.Write(_icon);
+            Engine.Render(_icon, Position);
         }
 
         public void End()
--- MathForGames/Engine.cs
+++ MathForGames/Engine.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using MathLibrary;
 
 namespace MathForGames
 {
@@ -9,7 +10,8 @@ namespace MathForGames
     {
         private static bool _applicationShouldClose = false;
         private static int _currentSceneIndex;
-        private Scene[] _scenes = new Scene[0];       
+        private Scene[] _scenes = new Scene[0];
+        private static Icon[,] _buffer;
 
         /// <summary>
         /// Called to begin the application
@@ -37,13 +39,19 @@ namespace MathForGames
         private void Start()
         {
             Scene scene = new Scene();
-            Actor actor = new Actor('P', new MathLibrary.Vector2 { X = 0, Y = 0 });
+            Actor actor = new Actor('P', new MathLibrary.Vector2 { X = 0, Y = 0 }, "Actor1", ConsoleColor.Yellow);
+            Actor actor2 = new Actor('A', new MathLibrary.Vector2 { X = 10, Y = 10 }, "Actor2", ConsoleColor.Green);
+            Player player = new Player('@', 5, 5, 1, "Player", ConsoleColor.DarkMagenta);
 
             scene.AddActor(actor);
+            scene.AddActor(actor2);
+            scene.AddActor(player);
 
             _currentSceneIndex = AddScene(scene);
 
             _scenes[_currentSceneIndex].Start();
+
+            Console.CursorVisible = false;
         }
         
         /// <summary>
@@ -59,8 +67,34 @@ namespace MathForGames
         /// </summary>
         private void Draw()
         {
-            Console.Clear();
+            //Clears the stuff that was on the screen in the last frame
+            _buffer = new Icon[Console.WindowWidth, Console.WindowHeight - 1];
+
+            //Reset the cursor position to the top so the previous screen is drawn over
+            Console.SetCursorPosition(0, 0);
+
+            //Adds all actor icons to buffer
             _scenes[_currentSceneIndex].Draw();
+
+            //Iterate through buffer
+            for (int y = 0; y < _buffer.GetLength(1); y++)
+            {
+                for (int x = 0; x < _buffer.GetLength(0); x++)
+                {
+                    if (_buffer[x, y].Symbol == '\0')
+                    {
+                        _buffer[x, y].Symbol = ' ';
+                    }
+
+                    //Set console text color to be color of item at buffer
+                    Console.ForegroundColor = _buffer[x, y].Color;
+                    //Print the symbol of the item in the buffer
+                    Console.Write(_buffer[x, y].Symbol);
+                }
+
+                //Skip a line once the end of a row had been reached
+                Console.WriteLine();
+            }
 
         }
 
@@ -96,6 +130,40 @@ namespace MathForGames
 
             //Return the last index
             return _scenes.Length - 1;
+        }
+
+        /// <summary>
+        /// Gets the next key in the input stream
+        /// </summary>
+        /// <returns>The key that was pressed</returns>
+        public static ConsoleKey GetNextKey()
+        {
+            //If there is no key being pressed...
+            if (!Console.KeyAvailable)
+                //...return
+                return 0;
+
+            //Return the current key being pressed
+            return Console.ReadKey(true).Key;
+        }
+
+        /// <summary>
+        /// Adds the icon to the buffer to print to the screen in the next draw call.
+        /// Prints the icon at the given position in the buffer.
+        /// </summary>
+        /// <param name="icon">The icon to draw</param>
+        /// <param name="position">The position of the icon in the buffer</param>
+        /// <returns>False if the position is outside the bounds of the buffer</returns>
+        public static bool Render(Icon icon, Vector2 position)
+        {
+            //If the position is out of bounds...
+            if (position.X < 0 || position.X >= _buffer.GetLength(0) || position.Y < 0 || position.Y >= _buffer.GetLength(1))
+                //...return false
+                return false;
+
+            //Set the buffer at the index of the given postition to be the icon
+            _buffer[(int)position.X, (int)position.Y] = icon;
+            return true;
         }
     }
 }
